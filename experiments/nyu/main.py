71dc33590518274721dc78a047325149bb647d3d
Anna Wieteska <209614@edu.p.lodz.pl>
2021-11-02 12:01:38 +0100

    Update traing and testing scripy V2V on NYUDataset

--- experiments/nyu/main.py
+++ experiments/nyu/main.py
@@ -1,5 +1,11 @@
 """
-## This script is used for testing the NYU hand dataset
+## This script is for run tesing and test NYU dataset
+"""
+
+# %matplotlib inline
+
+"""
+## This script is for run tesing and test MSRA dataset
 """
 
 # %matplotlib inline
@@ -17,26 +23,7 @@ from lib.solver import train_epoch, val_epoch, test_epoch
 from lib.sampler import ChunkSampler
 from src.v2v_model import V2VModel
 from src.v2v_util import V2VVoxelization
-from datasets.nyu_hand import pixel2world, world2pixel, depthmap2points, points2pixels, load_depthmap
-from datasets.nyu_hand import NYUDataset
-
-
-""
-def transform_test(sample):
-    points, refpoint = sample['points'], sample['refpoint']
-    input = voxelize_input(points, refpoint)
-    return torch.from_numpy(input), torch.from_numpy(refpoint.reshape((1, -1)))
-
-#######################################################################################
-# Note,
-# Run in project root direcotry(ROOT_DIR) with:
-# PYTHONPATH=./ python experiments/msra-subject3/main.py
-#
-# This script will train model on MSRA hand datasets, save checkpoints to ROOT_DIR/checkpoint,
-# and save test results(test_res.txt) and fit results(fit_res.txt) to ROOT_DIR.
-#
-
-checkpoint_dir = r'./checkpoint'
+from datasets.nyu_hand import NYUDataset_train, NYUDataset
 
 
 #######################################################################################
@@ -68,26 +55,66 @@ resume_after_epoch = args.resume
 
 save_checkpoint = True
 checkpoint_per_epochs = 1
-checkpoint_dir = r'./checkpoint'
+checkpoint_dir = r'./checkpoint_nyu'
 
 start_epoch = 0
 epochs_num = 15
 
 batch_size = 6
 
+###############################################################################
+# ls '/V2V-PoseNet/V2V-PoseNet-pytorch/datasets/train_bin/joint_data.mat' 
+
+###############################################################################
+# ls '/V2V-PoseNet/V2V-PoseNet-pytorch/datasets/nyu_center/center_train_refined.txt' 
+
 #######################################################################################
 # # Data, transform, dataset and loader
 # Data
 print('==> Preparing data ..')
-
+data_dir = '/V2V-PoseNet/V2V-PoseNet-pytorch/datasets/train_bin/'
+center_dir = '/V2V-PoseNet/V2V-PoseNet-pytorch/datasets/nyu_center/'
 keypoints_num = 21
 test_subject_id = 3
-cubic_size = 200
+cubic_size = 250
 
 
 # Transform
-voxelization_train = V2VVoxelization(cubic_size=200, augmentation=True)
-voxelization_val = V2VVoxelization(cubic_size=200, augmentation=False)
+voxelization_train = V2VVoxelization(cubic_size=250, augmentation=True)
+voxelization_val = V2VVoxelization(cubic_size=250, augmentation=False)
+
+def transform_test(sample):
+    points, refpoint = sample['points'], sample['refpoint']
+    input = voxelize_input(points, refpoint)
+    return torch.from_numpy(input), torch.from_numpy(refpoint.reshape((1, -1)))
+
+def transform_train(sample):
+    points, keypoints, refpoint = sample['points'], sample['joints'], sample['refpoint']
+    assert(keypoints.shape[0] == keypoints_num)
+    input, heatmap = voxelization_train({'points': points, 'keypoints': keypoints, 'refpoint': refpoint})
+    return (torch.from_numpy(input), torch.from_numpy(heatmap))
+
+
+def transform_val(sample):
+    points, keypoints, refpoint = sample['points'], sample['joints'], sample['refpoint']
+    assert(keypoints.shape[0] == keypoints_num)
+    input, heatmap = voxelization_val({'points': points, 'keypoints': keypoints, 'refpoint': refpoint})
+    return (torch.from_numpy(input), torch.from_numpy(heatmap))
+
+
+# Dataset and loader
+
+train_set = NYUDataset_train(data_dir, center_dir ,transform_train)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=3)
+#train_num = 1
+#train_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=False, num_workers=6,sampler=ChunkSampler(train_num, 0))
+
+
+# No separate validation dataset, just use test dataset instead
+root = '/V2V-PoseNet/V2V-PoseNet-pytorch/datasets/test_bin'
+val_set = NYUDataset(root, center_dir, transform_test)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=6)
+
 
 #######################################################################################
 # # Model, criterion and optimizer
@@ -105,90 +132,100 @@ criterion = nn.MSELoss()
 optimizer = optim.Adam(net.parameters())
 #optimizer = optim.RMSprop(net.parameters(), lr=2.5e-4)
 
+#######################################################################################
+# # Resume
+if resume_train:
+    # Load checkpoint
+    epoch = resume_after_epoch
+    checkpoint_file = os.path.join(checkpoint_dir, 'epoch'+str(epoch)+'.pth')
 
-""
-# Resume
-# if resume_train:
+    print('==> Resuming from checkpoint after epoch {} ..'.format(epoch))
+    assert os.path.isdir(checkpoint_dir), 'Error: no checkpoint directory found!'
+    assert os.path.isfile(checkpoint_file), 'Error: no checkpoint file of epoch {}'.format(epoch)
 
-# Load checkpoint
-# epoch = resume_after_epoch
-# checkpoint_file = os.path.join(checkpoint_dir, 'epoch'+str(epoch)+'.pth')
-
-# print('==> Resuming from checkpoint after epoch {} ..'.format(epoch))
-# assert os.path.isdir(checkpoint_dir), 'Error: no checkpoint directory found!'
-# assert os.path.isfile(checkpoint_file), 'Error: no checkpoint file of epoch {}'.format(epoch)
-
-# checkpoint = torch.load(os.path.join(checkpoint_dir, 'epoch'+str(epoch)+'.pth'))
-print(checkpoint_dir)
-checkpoint=torch.load('/V2V-PoseNet/V2V-PoseNet-pytorch/checkpoint/epoch14.pth')
-net.load_state_dict(checkpoint['model_state_dict'])
-optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-# start_epoch = checkpoint['epoch'] + 1
-
+    checkpoint = torch.load(os.path.join(checkpoint_dir, 'epoch'+str(epoch)+'.pth'))
+    net.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    start_epoch = checkpoint['epoch'] + 1
 
 
 #######################################################################################
+# # Train and Validate
+print('==> Training ..')
+for epoch in range(start_epoch, start_epoch + epochs_num):
+    print('Epoch: {}'.format(epoch))
+    train_epoch(net, criterion, optimizer, train_loader, device=device, dtype=dtype)
+    val_epoch(net, criterion, val_loader, device=device, dtype=dtype)
+
+    if save_checkpoint and epoch % checkpoint_per_epochs == 0:
+        if not os.path.exists(checkpoint_dir): os.mkdir(checkpoint_dir)
+        checkpoint_file = os.path.join(checkpoint_dir, 'epoch'+str(epoch)+'.pth')
+        checkpoint = {
+            'model_state_dict': net.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'epoch': epoch
+        }
+        torch.save(checkpoint, checkpoint_file)
+
+
+""
 # # Test
-print('==> Testing ..')
-voxelize_input = voxelization_train.voxelize
-evaluate_keypoints = voxelization_train.evaluate
+# print('==> Testing ..')
+# voxelize_input = voxelization_train.voxelize
+# evaluate_keypoints = voxelization_train.evaluate
+
+""
+# def transform_test(sample):
+#     points, refpoint = sample['points'], sample['refpoint']
+#     input = voxelize_input(points, refpoint)
+#     return torch.from_numpy(input), torch.from_numpy(refpoint.reshape((1, -1)))
 
 
-def transform_test(sample):
-    points, refpoint = sample['points'], sample['refpoint']
-    input = voxelize_input(points, refpoint)
-    return torch.from_numpy(input), torch.from_numpy(refpoint.reshape((1, -1)))
+# def transform_output(heatmaps, refpoints):
+#     keypoints = evaluate_keypoints(heatmaps, refpoints)
+#     return keypoints
 
 
-def transform_output(heatmaps, refpoints):
-    keypoints = evaluate_keypoints(heatmaps, refpoints)
-    return keypoints
-
-
-class BatchResultCollector():
-    def __init__(self, samples_num, transform_output):
-        self.samples_num = samples_num
-        self.transform_output = transform_output
-        self.keypoints = None
-        self.idx = 0
+# class BatchResultCollector():
+#     def __init__(self, samples_num, transform_output):
+#         self.samples_num = samples_num
+#         self.transform_output = transform_output
+#         self.keypoints = None
+#         self.idx = 0
     
-    def __call__(self, data_batch):
-        inputs_batch, outputs_batch, extra_batch = data_batch
-        outputs_batch = outputs_batch.cpu().numpy()
-        refpoints_batch = extra_batch.cpu().numpy()
+#     def __call__(self, data_batch):
+#         inputs_batch, outputs_batch, extra_batch = data_batch
+#         outputs_batch = outputs_batch.cpu().numpy()
+#         refpoints_batch = extra_batch.cpu().numpy()
 
-        keypoints_batch = self.transform_output(outputs_batch, refpoints_batch)
+#         keypoints_batch = self.transform_output(outputs_batch, refpoints_batch)
 
-        if self.keypoints is None:
-            # Initialize keypoints until dimensions awailable now
-            self.keypoints = np.zeros((self.samples_num, *keypoints_batch.shape[1:]))
+#         if self.keypoints is None:
+#             # Initialize keypoints until dimensions awailable now
+#             self.keypoints = np.zeros((self.samples_num, *keypoints_batch.shape[1:]))
 
-        batch_size = keypoints_batch.shape[0] 
-        self.keypoints[self.idx:self.idx+batch_size] = keypoints_batch
-        self.idx += batch_size
+#         batch_size = keypoints_batch.shape[0] 
+#         self.keypoints[self.idx:self.idx+batch_size] = keypoints_batch
+#         self.idx += batch_size
 
-    def get_result(self):
-        return self.keypoints
-
-
-print('Test on test dataset ..')
-def save_keypoints(filename, keypoints):
-    # Reshape one sample keypoints into one line
-    keypoints = keypoints.reshape(keypoints.shape[0], -1)
-    np.savetxt(filename, keypoints, fmt='%0.4f')
+#     def get_result(self):
+#         return self.keypoints
 
 
-center_dir = '/V2V-PoseNet/V2V-PoseNet-pytorch/datasets/nyu_center/'
-ref_pt_file = 'test_center_uvd.txt'
-root = '/V2V-PoseNet/V2V-PoseNet-pytorch/datasets/test_bin'
+# print('Test on test dataset ..')
+# def save_keypoints(filename, keypoints):
+#     # Reshape one sample keypoints into one line
+#     keypoints = keypoints.reshape(keypoints.shape[0], -1)
+#     np.savetxt(filename, keypoints, fmt='%0.4f')
 
-test_set = NYUDataset(root, center_dir, 'test', transform_test)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=6)
-test_res_collector = BatchResultCollector(len(test_set), transform_output)
 
-test_epoch(net, test_loader, test_res_collector, device, dtype)
-keypoints_test = test_res_collector.get_result()
-save_keypoints('./test_res.txt', keypoints_test)
+# test_set = MARAHandDataset(data_dir, center_dir, 'test', test_subject_id, transform_test)
+# test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=6)
+# test_res_collector = BatchResultCollector(len(test_set), transform_output)
+
+# test_epoch(net, test_loader, test_res_collector, device, dtype)
+# keypoints_test = test_res_collector.get_result()
+# save_keypoints('./test_res.txt', keypoints_test)
 
 
 # print('Fit on train dataset ..')
@@ -200,7 +237,57 @@ save_keypoints('./test_res.txt', keypoints_test)
 # keypoints_fit = fit_res_collector.get_result()
 # save_keypoints('./fit_res.txt', keypoints_fit)
 
-print('All done ..')
+# print('All done ..')
+
+###############################################################################
+# import os
+# import numpy as np
+# import sys
+# import struct
+# from torch.utils.data import Dataset
+# import scipy.io as scio
+
+###############################################################################
+# num_samples = 72757
+# world_dim = 3
+# joint_num = 21
+
+###############################################################################
+# joints_world = np.zeros((num_samples, joint_num, world_dim))
+
+###############################################################################
+#  = '/V2V-PoseNet/V2V-PoseNet-pytorch/datasets/train_bin/joint_data.mat'
+
+###############################################################################
+# root = '/V2V-PoseNet/V2V-PoseNet-pytorch/datasets/train_bin/'
+
+###############################################################################
+# keypointsXYZ_test = scio.loadmat(keypoint_file)["joint_xyz"].astype(np.float32)[0]
+
+###############################################################################
+# EVAL_JOINTS = np.array([
+#     0, 6, 12, 18, 24,
+#     1, 7, 13, 19, 25,
+#     4, 10, 15, 21, 26,
+#     5, 11, 17, 23, 28,
+#     29 ])
+
+###############################################################################
+# keypointsXYZ_test = keypointsXYZ_test[::][:,EVAL_JOINTS,:]
+
+###############################################################################
+# np.shape(keypointsXYZ_test)
+
+###############################################################################
+# np.shape(joints_world)
+
+###############################################################################
+# for fileID in range(0, 72757):
+#     joints_world[fileID] = keypointsXYZ_test[fileID]
+#     print(os.path.join(root, 'depth_1_{:0>7d}.bin'.format(fileID+1)))
+
+###############################################################################
+# ls /V2V-PoseNet/V2V-PoseNet-pytorch/datasets/train_bin/depth_1_0000001.bin
 
 ""
 
